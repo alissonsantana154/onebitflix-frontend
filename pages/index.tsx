95ceb8e542c972243c1247635daafe766d0349d4
Alisson Santana <alissonsantana16608@gmail.com>
2023-04-25 17:17:37 -0300

    Criação do header da página homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss"
+import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
   return (
@@ -10,7 +11,9 @@ const HomeNoAuth = () => {
         <meta property="og:title" content="Onebitflix" key="title" />
         <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"/>
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth/>
+      </main>
     </>
   );
 };
---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,39 @@
+import { Button, Container } from "reactstrap";
+import styles from "./styles.module.scss"
+import Link from "next/link";
+
+const HeaderNoAuth = function () {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        <img 
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+        <p>Se cadastre para ter acesso aos cursos</p>
+        <img src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+      </div>
+      <Container className={styles.nav}>
+        <img src="/logoOnebitflix.svg" alt="logoOnebitflix" className={styles.imgLogoNav}/>
+        <div>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Entrar
+            </Button>
+          </Link>
+          <Link href="/register">
+            <Button className={styles.navBtn} outline>
+              Quero fazer parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default HeaderNoAuth
