374e66a70034e60b50d6df0a06fceae4b1c8832d
Alisson Santana <alissonsantana16608@gmail.com>
2023-04-25 16:06:50 -0300

    Configurando pastas e componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"/>
       </Head>
       <main></main>
     </>
