c93d51b5aa8729f881e80928a5262502b7cc6800
Alisson Santana <alissonsantana16608@gmail.com>
2023-04-27 12:00:13 -0300

    Conexão com o Backend

---
+++ src/services/courseService.ts
@@ -0,0 +1,33 @@
+import { count, error } from "console";
+import api from "./api";
+
+export type EpisodeType = {
+  id: number;
+  name: string;
+  synopsis: string;
+  order: number;
+  videUrl: string;
+  secondsLong: number;
+};
+
+export type CourseType = {
+  id: number;
+  name: string;
+  thumbnailUrl: string;
+  synopsis: string;
+  episodes?: EpisodeType[];
+};
+
+const CourseService = {
+  getNewestCourses: async () => {
+    const res = await api.get("./courses/newest").catch((error) => {
+      console.log(error.response.data.message);
+
+      return error.response;
+    });
+    
+    return res;
+  },
+};
+
+export default CourseService
