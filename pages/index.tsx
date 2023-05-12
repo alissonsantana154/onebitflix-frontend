f9d23979ecba33e9f605c7c71341d3f317476adf
Alisson Santana <alissonsantana16608@gmail.com>
2023-05-12 13:56:01 -0300

    Finalização da estrutura dos slides - Slide Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,23 @@ import styles from "../styles/HomeNoAuth.module.scss"
 import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "../src/components/homeNoAuth/presentationSection";
 import CardsSection from "../src/components/homeNoAuth/cardsSection";
+import SlideSection from "../src/components/homeNoAuth/slideSection";
+import { GetStaticProps } from "next";
+import courseServices, { CourseType } from "../src/Services/courseService";
+import { ReactNode } from "react";
 
-const HomeNoAuth = () => {
+interface IndexPageProps {
+  chrildren?: ReactNode;
+  course: CourseType[];
+}
+
+const HomeNoAuth = ({ course }: IndexPageProps) => {
   return (
     <>
       <Head>
         <title>Onebitflix</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-        <meta property="og:title" content="Onebitflix" key="title" />
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
+        <meta property="og:title" content="Onebitflix" key="title"/>
         <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"/>
       </Head>
       <main>
@@ -19,9 +28,20 @@ const HomeNoAuth = () => {
         <PresentationSection/>
         </div>
         <CardsSection/>
+        <SlideSection newestCourses={course}/>
       </main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseServices.getNewestCourses();
+  return {
+    props: {
+      course: res.data,
+    },
+    revalidate: 3600 * 24,
+  };
 };
 
 export default HomeNoAuth;
