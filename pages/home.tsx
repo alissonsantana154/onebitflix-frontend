a4126bae5fdbd412848b8a9f3bf5a2cc5de87732
Alisson Santana <alissonsantana16608@gmail.com>
2023-05-19 14:25:19 -0300

    Criação da página home autenticada, do seu estilo visual e também da sua responsividade completa. Implementação de todas as categorias na página e criação de seriços - Home autenticada

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import FavoriteCategory from "../src/components/homeAuth/favoriteCategory";
 import FeaturedCategory from "../src/components/homeAuth/featuredCategory";
 import ListCategories from "../src/components/homeAuth/listCategories";
+import Footer from "../src/components/common/footer";
 
 const HomeAuth = function () {
   return (
@@ -18,6 +19,7 @@ const HomeAuth = function () {
         <FavoriteCategory/>
         <FeaturedCategory/>
         <ListCategories/>
+        <Footer/>
       </main>
     </>
   );
