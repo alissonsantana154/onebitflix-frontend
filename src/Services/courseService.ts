545e1ca6b690ae4a9b7c769b7fadfeb9b68fc942
Alisson Santana <alissonsantana16608@gmail.com>
2023-05-25 08:55:44 -0300

    Criação do método de search, comunicação com todo o front, criação de uma página para o seacrh - search

--- src/Services/courseService.ts
+++ src/Services/courseService.ts
@@ -86,6 +86,21 @@ const courseServices = {
   
       return res;
   },
+  getSearch: async (name: string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+  
+    const res = await api
+      .get(`/courses/search?name=${name}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {  
+        return error.response;
+      });
+  
+    return res;
+  },
 };
 
 export default courseServices;
--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -2,7 +2,7 @@ import { Container, Form, Input, } from "reactstrap";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import Modal from "react-modal";
-import { useEffect, useState } from "react"
+import { useEffect, useState, FormEvent } from "react"
 import { useRouter } from "next/router";
 import profileService from "../../../Services/profileService";
 
@@ -12,7 +12,20 @@ const HeaderAuth = function () {
   const router = useRouter();
   const [modalOpen, setModalOpen] = useState(false);
   const [initials, setInitials] = useState("");
+  const [searchName, setSearchName] = useState("");
 
+  const handleSearch = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    router.push(`/search?name=${searchName}`);
+    setSearchName("");
+  };
+
+  const handleSearchClick = () => {
+    router.push(`/search?name=${searchName}`);
+		setSearchName("");
+  };
+  
   useEffect(() => {
     profileService.fetchCurrent().then((user) => {
       const firstNameInitial = user.firstName.slice(0, 1);
@@ -24,15 +37,15 @@ const HeaderAuth = function () {
 const handleOpenModal = () => {
 	setModalOpen(true);
 };
-
 const handleCloseModal = () => {
 	setModalOpen(false);
 };
-
 const handleLogout = () => {
 	sessionStorage.clear();
+  
   router.push("/");
 };
+
   return (
     <>
       <Container className={styles.nav}>
@@ -43,18 +56,23 @@ const handleLogout = () => {
             className={styles.imgLogoNav}/>
         </Link>
         <div className="d-flex align-items-center">
-        <Form>
+        <Form onSubmit={handleSearch}>
           <Input
             name="search"
             type="search"
             placeholder="Pesquisar"
             className={styles.input}
+            value={searchName}
+            onChange={(event) => {
+              setSearchName(event.currentTarget.value.toLowerCase());
+            }}
           />
         </Form>
         <img
           src="/homeAuth/iconSearch.svg"
           alt="lupaHeader"
           className={styles.searchImg}
+          onClick={handleSearchClick}
         />
         <p className={styles.userProfile} onClick={handleOpenModal}>{initials}</p>
         </div>
